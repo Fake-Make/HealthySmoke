52b57cb30a1a02c7298819bbf946932964c09c1a
Dmitry Nevada <vegnerdima@yandex.ru>
2019-04-15 12:11:21 +0700

    Deviding into parts

--- registration.php
+++ registration.php
@@ -1,75 +1,6 @@
-<!DOCTYPE html>
-<html lang="ru">
-
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="css/stylesheet.css">
-	<link rel="shortcut icon" href="img/favicon.png" type="image/png">
-	<link rel="alternate" href="https://allfont.ru/allfont.css?fonts=arial-narrow">
-	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-	<script src="js/script.js"></script>
-	<title>Company - Интернет-магазин электронных сигарет</title>
-</head>
-
+<? require_once("inner/meta.php"); ?>
 <body>
-	<header class="page-header">
-		<div class="wrapper">
-			<aside class="header-top">
-				<div class="header-logo">
-					<img class="header-logo__image" src="img/logo.png" alt="Логотип" width="95" height="75">
-					<span class="header-logo__caption">Company</span>
-				</div>
-				<div class="company-info">
-					<b class="company-info__tagline">Нанотехнологии здоровья</b>
-					<div class="contacts">
-						<a class="contacts__link-mail" href="mailto:info@company.ru">info@company.ru</a>
-						<a class="contacts__link-phone" href="tel:+73833491849">+7 (383) 349-18-49</a>
-					</div>
-				</div>
-			</aside>
-			<div class="user-info">
-				<form class="user-info__form" method="POST" action="send">
-					<span>
-						<label class="user-info__label" for="user-name">Логин: </label>
-						<input class="user-info__input" type="text" name="user-name" id="user-name">
-					</span>
-					<span>
-						<label class="user-info__label user-info__label_margin-right_bigger" for="user-password">Пароль: </label>
-						<input class="user-info__input" type="password" name="user-password" id="user-password">
-					</span>
-					<input class="user-info__link" type="submit" value="Войти">
-				</form>
-				<span class="user-info__link user-info__link_reposition header-nav-item__link_current">Регистрация</span>
-				<span class="cart-label">В <a class="cart-label__link" href="#">корзине</a> товаров - <b>12</b></span>
-			</div>
-		</div>
-		<nav class="header-nav">
-			<div class="wrapper">
-				<span class="menu-toggler">Меню</span>
-				<ul class="menu-togglable">
-					<li class="header-nav-item"><span><a class="header-nav-item__link" href="index.html">Главная</a></span></li>
-					<li class="header-nav-item">
-						<span class="header-nav-item__container-for-link"><a class="header-nav-item__link" href="catalog.html">Каталог</a></span>
-						<ul class="sub-menu">
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Электронные сигареты</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Трубки</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Картриджи</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Аккумуляторы и атомайзеры</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Аксессуары</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Зарядные устройства</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Жидкости для заправки</a></li>
-							<li class="sub-menu__list-item"><a class="sub-menu__link" href="#">Подарочные наборы</a></li>
-						</ul>
-					</li>
-					<li class="header-nav-item"><span><a class="header-nav-item__link" href="#">О компании</a></span></li>
-					<li class="header-nav-item"><span><a class="header-nav-item__link" href="#">Новости</a></span></li>
-					<li class="header-nav-item"><span><a class="header-nav-item__link" href="shipment.html">Доставка и оплата</a></span></li>
-					<li class="header-nav-item"><span><a class="header-nav-item__link" href="contacts.html">Контакты</a></span></li>
-				</ul>
-			</div>
-		</nav>
-	</header>
+	<? require_once("inner/header.php"); ?>
 	<div class="content">
 		<div class="wrapper content__wrapper">
 			<main class="inside-content inside-content__registration">
@@ -195,30 +126,7 @@
 			</div>
 		</div>
 	</div>
-	<footer class="page-footer">
-		<div class="wrapper page-footer__wrapper">
-			<div class="copyright">
-				<span class="copyright__part copyright__lifetime">Copyright ©2007-2010</span>
-				<span class="copyright__part copyright__company-lifetime"><b>© "Company"</b>, 2010</span>
-				<img class="copyright__image" src="img/logo.png" alt="Company-logo">
-				<span class="copyright__part copyrhigt__company-name">Company</span>
-			</div>
-			<nav class="footer-nav">
-				<ul class="footer-nav__list">
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="index.html">Главная</a></li>
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="catalog.html">Каталог</a></li>
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="#">О компании</a></li>
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="#">Новости</a></li>
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="shipment.html">Доставка и оплата</a></li>
-					<li class="footer-nav__list-item"><a class="footer-nav__link" href="contacts.html">Контакты</a></li>
-				</ul>
-			</nav>
-			<div class="developer">
-				<span class="developer__ref">Разработка сайта - <a class="developer__link" href="#">ITConstruct</a></span>
-				<img class="counter" src="img/counter.png" alt="Page-counter">
-			</div>
-		</div>
-	</footer>
+	<? require_once("inner/footer.php"); ?>
 </body>
 
 </html>
